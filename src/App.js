663a08d534d81e1ba28acf3a8ecf657ff20d444b
Jaeyoon1991 <rainbowcrayon91@gmail.com>
2025-02-03 12:05:56 +0900

    #7.0 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import PropTypes from "prop-types";
-import style from "./App.module.css";
-import Button from "./Button";
+import { useState } from "react";
 
 
 function App() {
+  const[toDo, setToDo] =  useState("");
+  const[toDos, setToDos] = useState([]);
+  const onChagne = (event) => setToDo(event.target.value);
+  console.log(toDo);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]); 
+    setToDo("");
+  };
+  console.log(toDos);
   return (
     <div>
-      <h1 className={style.title}>Welcome back ReactJS</h1>
-      <Button text={"Continue"}/>
+      <h1>My To Dos : {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+       <input onChange={onChagne} value={toDo} type="text" placeholder="Wrtie your to do...."></input>
+       <button>Add to do..</button>
+
+      </form>
+
     </div>
   );
 }
